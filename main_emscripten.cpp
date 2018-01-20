5577676635250665a4d310dedf06d17b2ecd849d
Philip Wardlaw <wardlawp@tcd.ie>
2018-01-19 23:38:24 -0800

    Added emscripten main file and html front end. Modified build to support emscripten build

--- main_emscripten.cpp
+++ main_emscripten.cpp
@@ -1,34 +1,41 @@
 #ifndef  __EMSCRIPTEN__
    #error Emscripten is required
-#endif // ! __EMSCRIPTEN__
+#endif 
 
+// Modify the following two definitions to target page elements
+// Input should be a textarea
+// Output should be a div
+#define INPUT_ID "input"
+#define OUTPUT_ID "output"
 
 #include <emscripten.h>
 #include <emscripten/html5.h>
+
 #include <Tokenizer.h>
 #include <Parser.h>
 #include <Interpreter.h>
 #include <Exceptions.h>
 
+#include <algorithm>
 #include <sstream>
-#include <exception>
 #include <queue>
 
 
 using namespace std;
 using namespace JarJar;
 
+const int FPS = 20;
+char buff[4096];
 auto output = queue<string>();
-
+string latestInput;
+Interpreter* i;
 
 // Forward Declarations
 bool processInput();
 void print();
 void log(string);
-void oneIter();
-string latestInput;
-
-Interpreter* i;
+void loopIteration();
+void replaceEndls(string&);
 
 int main(int argc, char *argv[])
 {
@@ -36,13 +43,13 @@ int main(int argc, char *argv[])
 
    i = new Interpreter(&output);
 
-   emscripten_set_main_loop(oneIter, 5, 1);
+   emscripten_set_main_loop(loopIteration, FPS, 1);
    
    delete i;
    return 0;
 }
 
-void oneIter()
+void loopIteration()
 {
       try
       {
@@ -52,38 +59,48 @@ void oneIter()
          }
         
          log("Running Statement");
-         
+         output.push(latestInput);
 
          Tokenizer t = Tokenizer(latestInput);
          Parser p = Parser(t.getTokens());
 
          i->interpert(p.eval());
-         print();	
-
+        
       }
       catch (const runtime_error &e)
       {
          stringstream msgStream;
-         msgStream << "Caught runtime error " << type(e);
-
-         string line1, line2;
-         line1 = msgStream.str();
-         line2 = e.what();
-
-         log(line1);
-         log(line2);
-         output.push(line1);
-         output.push(line2);
+         msgStream << "Caught runtime error " << type(e) << endl << e.what();
+         string msg = msgStream.str();
+         log(msg);
+         output.push(msg);
       }
+
+      print();
 }
 
-void log(string input) {
-   string cmd = "console.log('" +  input  + "');";
-   emscripten_run_script(cmd.c_str());
+// Javascript
+char c_log[]      = "console.log('%s');";
+char getInput[]   = "document.getElementById('" INPUT_ID "').value;";
+char wipeInput[]  = "document.getElementById('" INPUT_ID "').value = '';";
+char beginOutput[]= "var input = document.getElementById('" INPUT_ID "');";
+char scroll[]     = "var el = document.getElementById('" OUTPUT_ID "'); el.scrollTop = el.scrollHeight;";
+char c_addLine[]  = "var line = document.createElement('p'); line.innerHTML = '%s'; "  
+                    "input.parentNode.insertBefore(line, input);";
+
+void log(string input) 
+{
+   replaceEndls(input);
+   sprintf(buff, c_log, input.c_str());
+   emscripten_run_script(buff);
 }
 
-char setOutput[] = 
-"var ul = document.getElementById(\"output\"); var li;";
+void addline(stringstream& builder, string& content)
+{
+   replaceEndls(content);
+   sprintf(buff, c_addLine, content.c_str());
+   builder << ' ' << buff << ' ';
+}
 
 void print()
 {
@@ -92,31 +109,41 @@ void print()
    }
 
    stringstream builder;
-   builder << setOutput;
+   builder << beginOutput;
+
    while(!output.empty())
    {
-      builder << "li = document.createElement(\"li\"); \
-                  li.appendChild(document.createTextNode(\"" 
-              << output.front() << "\")); \
-                  ul.appendChild(li);";
-
+      addline(builder, output.front());
       output.pop();
    }
+
    emscripten_run_script(builder.str().c_str());
+   emscripten_run_script(scroll);
 }
 
-char getInputJS[] = "document.getElementById(\'input\').value;";
-char wipeInput[] = "document.getElementById(\'input\').value = '';";
-int counter = 0;
 
+// returns true if there is input ready to process
 bool processInput()
 {
-   string received = emscripten_run_script_string(getInputJS);
-   if(received.back() == '\n') { 
-      latestInput = received.substr(counter,received.size() -1);
+   string received = emscripten_run_script_string(getInput);
+
+   auto pos = received.find('\n'); 
+
+   if(pos != std::string::npos) {
+      latestInput = received.substr(0,pos);
       emscripten_run_script(wipeInput);
       return true;
    }  
 
    return false;
+}
+
+// For JS encoding purposes
+void replaceEndls(string& input)
+{
+   string::size_type pos = 0; // Must initialize
+   while ((pos = input.find("\n", pos)) != string::npos)
+   {
+      input.replace(pos, 1, "\\n");
+   }
 }
